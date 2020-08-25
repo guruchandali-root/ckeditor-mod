7acc143b751752eeb7ff6fe252f399953df88d77
Rabimba Karanjai <karanjai.moz@gmail.com>
2020-08-25 15:12:54 +0530

    updated upload message

--- editor/ckeditor/plugins/imgur/lang/en.js
+++ editor/ckeditor/plugins/imgur/lang/en.js
@@ -1,6 +1,6 @@
 CKEDITOR.plugins.setLang('imgur', 'en', {
     clientIDMissing: 'Add config.imgurClientID to your assets/javascripts/ckeditor/config.js',
-    uploading: ' images are uploading...',
-    failToUpload: 'Failed to upload:',
+    uploading: 'গুরু চন্ডাল দের খুঁজে বার করতে গেছে ...',
+    failToUpload: 'চন্ডাল খুঁজে পাওয়া গেলোনা:',
     label: 'upload images'
 });
